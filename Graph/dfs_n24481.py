f8962febeb846b835cdcf247a157fe29021e7610
ZinZero <realbloom0122@gmail.com>
2023-01-11 17:15:12 +0900

    update 230111

---
+++ Graph/dfs_n24481.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+N, M, R = map(int, sys.stdin.readline().split())
+
+matrix = [[0] * (N + 1) for _ in range(N + 1)]
+depth = [-1] * (N + 1)
+count = 0
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    matrix[a][b] = matrix[b][a] = 1
+
+def dfs(V):
+    global count
+    depth[V] = count
+    for g in range(1, N + 1):
+        if depth[g] == -1 and matrix[V][g] == 1:
+            count += 1
+            dfs(g)
+
+dfs(R)
+
+for i in range(1, N + 1):
+    print(depth[i])
+
+# 메모리 초과... 
