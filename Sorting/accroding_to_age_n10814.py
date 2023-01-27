0954d692fc56bec040e90dd0f1a33368c609f755
ZinZero <realbloom0122@gmail.com>
2023-01-27 09:38:43 +0900

    update 230127

--- Sorting/accroding_to_age_n10814.py
+++ Sorting/accroding_to_age_n10814.py
@@ -5,13 +5,15 @@ N = int(sys.stdin.readline().strip())
 arr = []
 
 for _ in range(N):
-    line = sys.stdin.readline().strip()
-    # arr.append(line.split())
-    arr.append(line)
+    age, name = sys.stdin.readline().split()
+    arr.append((int(age), name))
 
-arr.sort()
+# 버블정렬 하면 시간초과 뜸 ㅠㅠ
+# for i in range(len(arr), 1, -1):
+#     for j in range(i - 1):
+#         if int(arr[j][0]) > int(arr[j + 1][0]):
+#             arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
+arr.sort(key=lambda x: x[0])    # lambda 를 이용해서 나이순 정렬
 for i in arr:
-    print(i)
-
-
+    print(i[0], i[1])
